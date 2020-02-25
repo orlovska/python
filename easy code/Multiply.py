e6f5278d02bde2e7dae6f44610a46a3fd6245aca
orlovska <58840952+orlovska@users.noreply.github.com>
2020-02-24 16:33:56 -0800

    Upgraded example

--- easy code/Multiply.py
+++ easy code/Multiply.py
@@ -9,29 +9,46 @@
 # Hint: Add using bitwise operations; multiply using shift-and-add.
 
 def multiply(x, y):
- #   multiply(2, 4)  10, 100
+ #   multiply(13, 9)  (1101, 1001)
    
-    def add(a, b):# (0, 1000)
+    def add(a, b):# (0, 1001) (1001, 100100)  (101101, 1001000)
         running_sum, carryin, k, temp_a, temp_b = 0, 0, 1, a, b 
-        while temp_a or temp_b:#(1,2,3,4)
+        while temp_a or temp_b:#(1, 2, 3, 4) (1, 2, 3, 4, 5, 6) (1, 2, 3, 4, )
             ak, bk = a & k, b & k
-            # ak = 0, 0, 0, 0
-            # bk = 0, 0, 0, 1000
+            # ak1 = 0, 0, 0, 0
+            # bk1 = 1, 0, 0, 1000
+            # ak2 = 1, 0, 0, 1000, 0, 0
+            # bk2 = 0, 0, 100, 0, 0, 100000
+            # ak3 = 1, 0, 100, 1000, 0, 100000, 0
+            # bk3 = 0, 0, 0, 1000, 0, 0, 1000000
             carryout = (ak & bk) | (ak & carryin) | (bk & carryin) 
-            # carryout = 0, 0, 0, 0
+            # carryout1 = 0, 0, 0 
+            # carryout2 = 0, 0, 0, 0
+            # carryout3 = 0, 0, 0, 1000, 0, 0, 0
             running_sum |= ak ^ bk ^ carryin
-            # running_sum  = 0, 0, 0, 1000
+            # running_sum1  = 1, 1, 1, 1001
+            # running_sum2 = 1, 1, 101, 1101, 1101, 101101
+            #  running_sum3 = 1, 1, 101, 101, 10101, 110101, 1110101
             carryin, k, temp_a, temp_b = (carryout << 1, k << 1, temp_a >> 1, temp_b >> 1)
-            # k = 10, 100, 1000
-            #  temp_a = 0, 0, 0, 0
-            # temp_b = 100, 10, 1, 0
-        return running_sum | carryin #1000
+            # k1 = 10, 100, 1000
+            # temp_a1 = 0, 0, 0
+            # temp_b1 = 100, 10, 1
+            # k2 = 10, 100, 1000, 10000, 100000
+            # temp_a2 = 100, 10, 1
+            # temp_b2 = 10010, 1001, 100, 10, 1, 0
+            # carryin3 = 0, 0, 0, 10000, 0, 0, 0
+            # k3 = 10, 100, 1000, 10000, 100000, 1000000, 10000000
+            # temp_a3 = 10110, 1011, 101, 10, 1, 0, 0
+            # temp_b3 = 100100, 10010, 1001, 100, 10, 1, 0
+        return running_sum | carryin #1001, 101101, 1110101
     running_sum = 0
     while x: # Examines each bit of x. (1, )
-        if x & 1: #(1,)
+        if x & 1: #(1, 2)
             running_sum = add(running_sum, y)
-            # running_sum = add(0, 1000) = 1000
+            # running_sum1 = add(0, 1001) = 1001
+            #running_sum2 = add(1000, 100100) = 101101
+            #running_sum3 = add(101101, 1001000) = 1110101
         x, y = x >> 1, y << 1  
-        # x = 1, 0
-        # y = 1000, 10000
-    return running_sum #1000 = 8
+        # x = 110, 11, 1, 0
+        # y = 10010(2 + 16 = 18), 100100, 1001000, 10010000
+    return running_sum #1110101
