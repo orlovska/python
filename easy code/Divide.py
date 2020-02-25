82067b60d565cf3aeb3692c7b7ddbda80504ad68
orlovska <58840952+orlovska@users.noreply.github.com>
2020-02-24 18:20:55 -0800

    Fixed comments for "divide" code

--- easy code/Divide.py
+++ easy code/Divide.py
@@ -7,16 +7,27 @@ def divide (x , y) :
     result, power = 0, 32 
     y_power = y << power 
     # y_power = 110000000000000000000000000000000...
-    while x >= y:
-        while y_power > x: #110 < 1001 power = 1
+    while x >= y: # 1001 > 11, 11 = 11
+        while y_power > x: # 1 (1100000000...> 1001) 
+            #                110 < 1001  with power = 1; 
+            #                2 (110 > 11)
+            #                 11 !< 11 with power = 0
             y_power >>= 1
-            # y_power = 11...(31)
+            # y_power1 = 11...(31)
+            # y_power2 = 11
             power -= 1
-            # power = 31
+            # power1 = 31
+            # power2 = 0
+        print(power)
         result += 1 << power
-        # result = 10
+        print(result)
+        # result = 10, 11
         x -= y_power 
+        print(x)
         # x  = 1001
         #     -0110
-        #      0001
+        #      0011
     return result 
+
+
+print(divide(9,3))
