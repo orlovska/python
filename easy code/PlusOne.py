2cbed462e0ff8454182143057e92a4d347704f96
orlovska <58840952+orlovska@users.noreply.github.com>
2020-02-25 09:21:38 -0800

    New version "PlusOne" code

--- easy code/PlusOne.py
+++ easy code/PlusOne.py
@@ -4,22 +4,20 @@
 class Solution(object):
     def plusOne(self, digits):
     
-        carry = 1
-        while carry == 1:
-            digits = digits[::-1]
-            for index,value in enumerate(digits):
-                
-                
-                if value == 9:
-                    carry = 1
-                    digits[index] = 0 
-                    if index == len(digits)-1:
-                        
-                        digits.append(1)
-                        carry = 0
-                        break
-                else:
-                    digits[index] = value+1
-                    carry = 0
-                    break
+    
+        digits = digits[::-1]
+        digits[0] += 1
+     
+        for index,value in enumerate(digits):
+            
+            if value != 10:
+                break
+            
+            digits[index] = 0 
+            if index == len(digits)-1:
+                digits.append(1)
+                break
+            digits[index + 1] += 1 
+            
         return digits[::-1]
+        
