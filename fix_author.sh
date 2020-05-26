7eb5dea00cab65c93605c988af8a4cf6f9782496
Olga Orlovska <58840952+orlovska@users.noreply.github.com>
2020-05-26 11:04:58 -0700

    All old code in one folder

---
+++ fix_author.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch -f --env-filter '
+
+OLD_EMAIL="olya@ivans-mbp.lan"
+CORRECT_NAME="orlovska"
+CORRECT_EMAIL="58840952+orlovska@users.noreply.github.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
